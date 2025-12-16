185eed9d02061b14fb411b6c858138de4b629bf3
unknown <santiagoaroca090@gmail.com>
2025-12-16 18:50:38 -0500

    Basico de Bucle for

---
+++ Bucles.py
@@ -0,0 +1,15 @@
+#ejemplo basico con bucles
+
+print("Bievenido a la lista de estudiantes")
+estudiantes=[]
+
+num_estudiantes=input("cuantos estudiantes deseas agregar: ")
+print("No se ingreso correctamente un numero")
+for i in range(int(num_estudiantes)):
+    estudiantes.append(input(f"Nombre del estudiante numero {i+1}: "))
+print("La lista de todos lo estudiantes es:")
+contador=0
+for j in estudiantes:
+    contador+=1
+    print(f"{contador}. Estudiante: {j}")
+
