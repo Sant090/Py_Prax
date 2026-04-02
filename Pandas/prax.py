eceb6a19efb3647ab8b5123cf5511bc0b9c5d576
Sant090 <santiagoaroca090@gmail.com>
2026-04-01 21:42:14 -0500

    grafica de datos desde base de datos

--- Pandas/prax.py
+++ Pandas/prax.py
@@ -1,7 +1,26 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np  
 
+"""
 df= pd.read_excel("Datos/Demanda_Energia_SIN_2025.xlsx")
 
 print(df.head())   
 
 df.to_csv("Datos/Practica.csv", index=False)
+"""
+
+df = pd.read_excel("Datos/Demanda_Energia_SIN_2025.xlsx", header=3)
+print(df.head())
+
+print(df["Generación kWh"])
+
+y=df["Generación kWh"]
+
+t=np.arange(len(y))
+
+plt.plot(t,y)
+plt.xlabel("Tiempo")
+plt.ylabel("Generación kWh")
+plt.title("Generación de energía a lo largo del tiempo") 
+plt.show()
